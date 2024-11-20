c7edcf3c3f8352575e5ee9aca657b23a5d749928
everdom <everdom@gmail.com>
2024-11-20 13:48:31 +0800

    update start on boot script to avoid errors and add some hints.

--- startup-as-admin.ps1
+++ startup-as-admin.ps1
@@ -1,12 +1,21 @@
 # Run the following commands in an Administrator powershell prompt. 
 # Be sure to specify the correct path to your desktop_switcher.ahk file. 
 
-Unregister-ScheduledTask SuperKeyboard 
+$taskName = "SuperKeyboard"  
+
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {  
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false  
+    Write-Host "Task '$taskName' has been unregistered."  
+} else {  
+    # Write-Host "Task '$taskName' does not exist."  
+}
 $A = New-ScheduledTaskAction -Execute "$PSScriptRoot\SuperKeyboard.exe"
 $T = New-ScheduledTaskTrigger -AtLogon
 $P = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
 $S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
 $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
 Register-ScheduledTask SuperKeyboard -InputObject $D
+Write-Host "Task '$taskName' has been registered."  
 Start-ScheduledTask SuperKeyboard 
+Write-Host "Task '$taskName' has started."  
 
